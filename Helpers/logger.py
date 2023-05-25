dfe292481ae621342ded1f3c56af08ec79073cb8
Owlish <denys.zhyhulin@avid.com>
2023-05-25 20:16:03 +0200

    Logger refactoring

--- Helpers/logger.py
+++ Helpers/logger.py
@@ -9,31 +9,30 @@ class Logger:
     """
     Loguru wrapper, more sinks to be added
     """
+    _instance = None
 
-    def __init__(self) -> None:
-        """
-            Initialize and configure loguru
-        """
-
-        log_format = ("<green>{time: YYYY-MM-DD HH:mm:ss}</green> | "
-                      "<level>{level: <5}</level> | "
-                      "<cyan>{name: ^15}</cyan>-><cyan>{function: ^15}</cyan> | "
-                      "<level>{message}</level>")
-
-        logger_config = {
-            "handlers": [
-                {"sink": sys.stdout, "format": log_format, "backtrace": True}
-            ],
-        }
-
-        logger.configure(**logger_config)
-
-        self._logger = logger
-
-    def get(self) -> object:
+    @staticmethod
+    def get() -> object:
         """Returns configured loguru object
 
         Returns:
             object: loguru logger
         """
-        return self._logger
+        if not Logger._instance:
+            log_format = ("<level>{level: <5}</level> | "
+                          "<cyan>{name: ^15}</cyan>-><cyan>{function: ^15}</cyan> | "
+                          "<level>{message}</level>")
+
+            logger_config = {
+                "handlers": [
+                    {"sink": sys.stdout, "format": log_format, "backtrace": True}
+                ],
+            }
+
+            logger.configure(**logger_config)
+            Logger._instance = logger
+            Logger._instance.info("Logger initialized")
+
+        return Logger._instance
+
+
